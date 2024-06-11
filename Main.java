57c78ccc4192aeb75b1eb7dc9eb9337390cb7ad9
Buffhermit <164672809+Buffhermit@users.noreply.github.com>
2024-06-11 02:17:11 -0700

    Update Main.java
    added calendar creation

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 package com.fitness.tracker;
 
+import java.util.Calendar;
+
 public class Main {
     public static void main(String[] args) {
         User user = new User("john_doe", "password123");
@@ -7,6 +9,11 @@ public class Main {
         
         if (authService.login(user.getUsername(), user.getPassword())) {
             System.out.println("Login successful!");
+            //create object instance
+            Calendar proCalendar = new Calendar();
+            
+            proCalendar.userSel();
+
         } else {
             System.out.println("Login failed!");
         }
