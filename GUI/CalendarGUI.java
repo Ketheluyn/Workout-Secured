0e4b77b30cbd2d1660c4c898211ebf93036d7c31
Buffhermit <164672809+Buffhermit@users.noreply.github.com>
2024-07-06 15:02:27 -0700

    Updated Calendar GUI
    Updated Calendar GUI for the properties and added a user for my testing

--- GUI/CalendarGUI.java
+++ GUI/CalendarGUI.java
@@ -4,29 +4,62 @@ import com.fitness.tracker.Functions.AuthService;
 import com.fitness.tracker.Functions.User;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel; 
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.Calendar;
 
 public class CalendarGUI extends JPanel {
 
 	private JTable workoutCalendar;
 	private DefaultTableModel wTable;
+<<<<<<< Updated upstream
 	private final User currentUser = AuthService.getCurrentUser();
+=======
+	private JComboBox<String> monthWorkoutBox;
+	private final String[] months = {"January", "February", "March", "April", "May", "June",
+			"July", "August", "September", "October", "November", "December"};
+>>>>>>> Stashed changes
 	
 	public CalendarGUI() {
 		
-		setLayout(new BorderLayout());		
+		setLayout(new BorderLayout());
+
+		//Initialize combobox
+		monthWorkoutBox = new JComboBox<>(months);
+		monthWorkoutBox.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				updateTableMonth(monthWorkoutBox.getSelectedIndex());
+			}
+		});
+
 		//Initialize workout calendar 
-		wTable = new DefaultTableModel(new Object[]{"Day", "Data"}, 0);
-		
-		for(int i = 1; i <= 31; i++) {
-			wTable.addRow(new Object[]{i, ""});
-		}
-		
+		wTable = new DefaultTableModel(new Object[]{"Day", "Workout Information"}, 0);
+
+
 		workoutCalendar = new JTable(wTable);
+		// Set margins using an empty border
+		workoutCalendar.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+		//sets day column to a smaller size
+		workoutCalendar.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+
+		TableColumn dayReadjust = workoutCalendar.getColumnModel().getColumn(0);
+		dayReadjust.setPreferredWidth(50);
+		dayReadjust.setResizable(false);
+
+		TableColumn woReadjust = workoutCalendar.getColumnModel().getColumn(1);
+		woReadjust.setResizable(true);
+		woReadjust.setPreferredWidth(900); // Initial width (adjust as needed)
+		woReadjust.setMinWidth(100);
+
 		JScrollPane scrollPane = new JScrollPane(workoutCalendar);
+		scrollPane.setBorder(new EmptyBorder(10,10,10,10));
 		
 		//Configure save button
 		JButton saveButton = new JButton("Save");
@@ -101,20 +134,44 @@ public class CalendarGUI extends JPanel {
 		buttonPanel.add(editButton);
 		buttonPanel.add(clearButton);
 		buttonPanel.add(saveButton);
+<<<<<<< Updated upstream
 		buttonPanel.add(userInfoButton);
 		buttonPanel.add(goalsGUIButton);
 		
+=======
+
+		add(monthWorkoutBox, BorderLayout.NORTH);
+>>>>>>> Stashed changes
 		add(scrollPane, BorderLayout.CENTER);
         add(buttonPanel, BorderLayout.SOUTH);
-		
+
+		//Open with first month
+		updateTableMonth(Calendar.getInstance().get(Calendar.MONTH));
 	}
-	
+
+	private void updateTableMonth(int month) {
+		wTable.setRowCount(0);
+		int daysInMonth = getDaysInMonth(month);
+
+		for(int i = 1; i <= getDaysInMonth(month); i++) {
+			wTable.addRow(new Object[]{i, ""});
+		}
+
+	}
+
+	private int getDaysInMonth(int month) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.MONTH, month);
+
+		return calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+	}
+
 	public DefaultTableModel getWorkoutCalendar() {
 		return wTable;
 		
 	}
 	
-	public void setWrokoutCalendar(DefaultTableModel wTable) {
+	public void setWorkoutCalendar(DefaultTableModel wTable) {
 		this.wTable = wTable;
 		
 	}
--- Tests/AuthServiceTest.java
+++ Tests/AuthServiceTest.java
@@ -1,10 +1,10 @@
 package com.fitness.tracker.Tests;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import com.fitness.tracker.Functions.AuthService;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class AuthServiceTest {
     @Test
